e3d677024cc854d0eb714e911f20be095e0e70c0
Franck Cheng <franck@FranckdeMacBook-Pro.local>
2024-11-02 15:06:37 +0800

    Added Chrome driver(MacOS-arm64) & Firefox driver(MacOS-arm64) & SeleniumTests.java will detect OS to select the right driver

--- src/test/java/academy/teenfuture/crse/Utility/SeleniumTests.java
+++ src/test/java/academy/teenfuture/crse/Utility/SeleniumTests.java
@@ -1,5 +1,6 @@
 package academy.teenfuture.crse.Utility;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
@@ -10,18 +11,74 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariDriver;
 
+/**
+ * Simple tests for opening different browsers
+ * 
+ * Last modified: 2024-11-2 2:56 PM
+ *
+ * @author Franck
+ */
 public class SeleniumTests {
+
+	// Chrome paths
+	public static final String PATH_CHROME_DRIVER_WIN_X86 = "\\src\\main\\resources\\driver\\ChromeDriver\\chromedriver.exe";
+	public static final String PATH_CHROME_DRIVER_MAC_ARM64 = "/src/main/resources/Driver/ChromeDriver/mac-arm64/chromedriver";
+	public static final String PATH_CHROME_APP_WIN_X86 = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+	public static ChromeOptions chromeOptions;
+
+	// Firefox paths
+	public static final String PATH_FIREFOX_DRIVER_WIN_X86 = "\\src\\main\\resources\\Driver\\FirefoxDriver\\geckodriver.exe";
+	public static final String PATH_FIREFOX_DRIVER_MAC_ARM64 = "/src/main/resources/Driver/FirefoxDriver/mac-arm64/geckodriver";
+	public static final String PATH_FIREFOX_APP_WIN_X86 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+	public static FirefoxOptions firefoxOptions;
+
+	// Edge paths
+	public static final String PATH_EDGE_DRIVER_WIN_X86 = "\\src\\main\\resources\\driver\\EdgeDriver\\chromedriver.exe";
+
+	@BeforeAll
+	public static void osDetectionAndPathAdjustment() {
+		String osName = System.getProperty("os.name").toLowerCase();
+
+		if (osName.contains("win")) {
+            // Windows settings
+			// Chrome (Win-x86)
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + PATH_CHROME_DRIVER_WIN_X86);
+            chromeOptions = new ChromeOptions();
+			chromeOptions.setBinary(PATH_CHROME_APP_WIN_X86);
+
+			// Firefox (Win-x86)
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + PATH_FIREFOX_DRIVER_WIN_X86);
+			firefoxOptions = new FirefoxOptions();
+			firefoxOptions.setBinary(PATH_FIREFOX_APP_WIN_X86);
+
+			// Edge (Win-x86)
+			System.setProperty("webdriver.edgeDriver.driver", System.getProperty("user.dir") + PATH_EDGE_DRIVER_WIN_X86);
+
+        } else if (osName.contains("mac")) {
+            // MacOS settings
+			// Chrome (MacOS-arm64)
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + PATH_CHROME_DRIVER_MAC_ARM64);
+			chromeOptions = new ChromeOptions();
+
+			// Firefox (MacOS-arm64)
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + PATH_FIREFOX_DRIVER_MAC_ARM64);
+			firefoxOptions = new FirefoxOptions();
+
+			// Edge (MacOS-arm64)
+
+			// Safari (MacOS-arm64)
+
+        } else {
+            // Handle other OSes or default path
+
+        }
+	}
+
 	// Set up of Firefox
 	@Disabled
 	@Test
 	public void getStart_Firefox() {
-		System.setProperty("webdriver.gecko.driver",
-				System.getProperty("user.dir") + "\\src\\main\\resources\\Driver\\FirefoxDriver\\geckodriver.exe");
-
-		FirefoxOptions options = new FirefoxOptions();
-		options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
-
-		WebDriver driver = new FirefoxDriver(options);
+		WebDriver driver = new FirefoxDriver(firefoxOptions);
 		// WevDriver driver = new FirefoxDriver(); //for default setting
 		driver.get("https://www.google.com.hk");
 		driver.close();
@@ -32,15 +89,7 @@ public class SeleniumTests {
 	@Disabled
 	@Test
 	public void getStart_Chrome() {
-		System.setProperty("webdriver.chrome.driver",
-				// should set your driver path in String, eg.
-				// "C://AutoTestGraspScrapeCrawl//selenium//Driver//chromedriver.exe"
-				System.getProperty("user.dir") + "\\src\\main\\resources\\driver\\ChromeDriver\\chromedriver.exe");
-
-		ChromeOptions options = new ChromeOptions();
-		options.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
-
-		WebDriver driver = new ChromeDriver(options);
+		WebDriver driver = new ChromeDriver(chromeOptions);
 		// WebDriver driver = new ChromeDriver(); //for default path
 		driver.get("https://www.google.com.hk");
 		driver.close();
@@ -49,11 +98,6 @@ public class SeleniumTests {
 	@Disabled
 	@Test
 	public void getStart_Edge() {
-		// should set your driver path in String, eg.
-		// "C://AutoTestGraspScrapeCrawl//selenium//driver//msedgedriver.exe"
-		System.setProperty("webdriver.edgeDriver.driver",
-				System.getProperty("user.dir") + "\\src\\main\\resources\\driver\\EdgeDriver\\chromedriver.exe");
-
 		WebDriver driver = new EdgeDriver();
 
 		driver.get("https://www.google.com.hk");
